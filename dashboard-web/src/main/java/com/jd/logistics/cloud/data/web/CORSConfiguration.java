1475350cbf5845a01735fa942d721951991cd3fc
hubin6 <hubin6@jd.com>
2017-11-07 18:00:38 +0800

    reformat code
    Signed-off-by: hubin6 <hubin6@jd.com>

--- dashboard-web/src/main/java/com/jd/logistics/cloud/data/web/CORSConfiguration.java
+++ dashboard-web/src/main/java/com/jd/logistics/cloud/data/web/CORSConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class CORSConfiguration extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*")
-                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(false).maxAge(3600);
     }
 }
