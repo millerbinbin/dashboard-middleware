2a6928c89e2489e576f62d1fd36c6857d1531f39
hubin6 <hubin6@jd.com>
2017-11-14 18:04:46 +0800

    refine code
    Signed-off-by: hubin6 <hubin6@jd.com>

--- dashboard-web/src/main/resources/metrics/sample1/number-day.sql
+++ dashboard-web/src/main/resources/metrics/sample1/number-day.sql
@@ -1,4 +1,4 @@
-SELECT '在库SKU数'as f1, '14,300' as v1,
+SELECT '在库SKU'as f1, '14,300' as v1,
 '日环比'as f2, 8.8 as v2,
 '周同比'as f3, -4.3 as v3,
 '昨日全天'as f4, '13,222' as v4,
--- dashboard-web/src/main/resources/metrics/sample1/number-week.sql
+++ dashboard-web/src/main/resources/metrics/sample1/number-week.sql
@@ -1,4 +1,4 @@
-SELECT '在库SKU数'as f1, '104,123' as v1,
+SELECT '在库SKU'as f1, '104,123' as v1,
 '周环比'as f2, 4.8 as v2,
 '年同比'as f3, -2.3 as v3,
 '整周数据'as f4, '103,468' as v4,
